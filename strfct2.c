e99619aaaa7555022b10ae951e5e48d52c3a9520
annacolleta <annacolletamtetwa@gmail.com>
2023-05-24 03:38:18 -0700

    strfct2.c

---
+++ strfct2.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: a string
+ * @s2: a string
+ * Return: The difference s1 - s2
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * _strncmp - compares the first n characters of two strings
+ * @s1: a string
+ * @s2: a string
+ * @n: number of characters to compare
+ *
+ * Return: The difference s1 - s2
+ */
+int _strncmp(const char *s1, const char *s2, int n)
+{
+	int a = 0, diff = 0;
+
+	while ((s1[a] && s2[a]) && a < n)
+	{
+		diff += s1[a] - s2[a];
+		a++;
+	}
+	return (diff);
+}
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *p = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (p);
+}
