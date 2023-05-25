a95611c39859101f26b1f91d6cc86f04e44a7f58
Omar <omar0642624692@gmail.com>
2023-05-24 18:33:24 -0700

    strfct2.c

--- strfct2.c
+++ strfct2.c
@@ -1,6 +1,45 @@
 #include "shell.h"
 
 /**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	int n = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[n])
+	{
+		if (s[n] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[n] >= 48 && s[n] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[n] - '0');
+			n++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		n++;
+	}
+
+	ni *= min;
+	return (ni);
+}
+
+/**
  * _strcmp - a function that compares two strings.
  * @s1: a string
  * @s2: a string
